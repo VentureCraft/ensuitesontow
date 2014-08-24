e7cc7df86f9c6afa11b578b52e10dd0a303b171a
Chris Duell <me@chrisduell.com>
2014-08-24 17:03:33 +1000

    env updates

--- app/config/staging/database.php
+++ app/config/staging/database.php
@@ -55,9 +55,9 @@ return array(
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => get_env('DB_NAME'),
-            'username' => get_env('DB_USER'),
-            'password' => get_env('DB_PASS'),
+            'database' => getenv('DB_NAME'),
+            'username' => getenv('DB_USER'),
+            'password' => getenv('DB_PASS'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
