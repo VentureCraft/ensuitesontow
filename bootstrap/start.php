1d8bffb56be84448ddd2e254c9890c9f2f2a7ff6
Chris Duell <me@chrisduell.com>
2014-08-24 17:07:35 +1000

    env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'dev' => array('villa'),
-	'staging' => array('misty-hymen'),
+    'dev' => array('villa'),
+    'staging' => array('misty-hymen'),
 
 ));
 
@@ -42,7 +42,7 @@ $env = $app->detectEnvironment(array(
 |
 */
 
-$app->bindInstallPaths(require __DIR__.'/paths.php');
+$app->bindInstallPaths(require __DIR__ . '/paths.php');
 
 /*
 |--------------------------------------------------------------------------
@@ -55,10 +55,10 @@ $app->bindInstallPaths(require __DIR__.'/paths.php');
 |
 */
 
-$framework = $app['path.base'].
-                 '/vendor/laravel/framework/src';
+$framework = $app['path.base'] .
+    '/vendor/laravel/framework/src';
 
-require $framework.'/Illuminate/Foundation/start.php';
+require $framework . '/Illuminate/Foundation/start.php';
 
 /*
 |--------------------------------------------------------------------------
